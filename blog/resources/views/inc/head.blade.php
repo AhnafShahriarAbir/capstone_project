729f9baf297514f45667ff95e4eef89869792898
AhnafShahriarAbir <s3577189@student.rmit.edu.au>
2019-05-09 19:26:33 +1000

    chngage

--- blog/resources/views/inc/head.blade.php
+++ blog/resources/views/inc/head.blade.php
@@ -24,5 +24,5 @@
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 
-<script async="" defer="" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDfVh4DjXDehml8bNOj0d6ph58uOLQumpA&callback=initMap"> </script>
+
 <script src="{{ asset('js/mapscript.js') }}" defer></script>
